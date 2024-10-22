3c2cf92bdf2c0b86c3a0782d39e47e9234545157
Kultup <102855411+Kultup@users.noreply.github.com>
2024-10-22 16:02:14 +0300

    Update config.py
    й

--- config.py
+++ config.py
@@ -1,13 +1,1 @@
-# config.py
 
-TOKEN = '7520694700:AAH0Q3BBvcYy0FAouG2WkuqFn4JJyEXLfME'
-ADMIN_CHAT_ID = '-1002325680637'  
-UPLOAD_FOLDER = "uploads"  
-
-
-ADMIN_NAMES = {
-    1635099899: "Сергій",        
-    6070910226: "Діма",          
-    "@Al1naSaenko": "Аліна",     
-    "@AlexeyB1": "Олексій"       
-}
