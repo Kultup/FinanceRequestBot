c47f08b973950de9711f56c23152cebc04a64d2a
Kultup <102855411+Kultup@users.noreply.github.com>
2024-11-08 12:15:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
 
-TOKEN = '7520694700:AAH0Q3BBvcYy0FAouG2WkuqFn4JJyEXLfME'
-ADMIN_CHAT_ID = '-1002325680637'
+TOKEN = 
+ADMIN_CHAT_ID = 
 UPLOAD_FOLDER = "uploads"
 
 ADMIN_NAMES = {
-    1635099899: "Сергій",
-    6070910226: "Діма",
-    "@Al1naSaenko": "Аліна",
-    "@AlexeyB1": "Олексій"
+    
 }
